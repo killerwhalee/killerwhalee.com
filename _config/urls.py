60e04a396f3fa212032930b67f05cedcd9c9884a
killerwhalee <killerwhalee@kaist.ac.kr>
2023-02-10 20:03:16 +0900

    Added url appname and several functions

--- _config/urls.py
+++ _config/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', include('home.urls')),
+    path('', lambda request: redirect('home/')),
     path('home/', include('home.urls')),
     path('common/', include('common.urls')),
 ]
