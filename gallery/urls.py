ab4ea7fb139e823f6711fb0492c291766f6f345e
killerwhalee <killerwhalee@kaist.ac.kr>
2023-03-10 13:08:10 +0900

    Implemented edit, delete features

--- gallery/urls.py
+++ gallery/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.explore_gallery, name='explore-gallery'),
     path('<int:explore_id>/', views.explore, name='explore'),
     path('create/', views.explore_create, name='explore-create'),
-    path('edit/<int:explore_id>/', views.explore_edit, name='explore-edit')
+    path('edit/<int:explore_id>/', views.explore_edit, name='explore-edit'),
+    path('delete/<int:explore_id>/', views.explore_delete, name='explore-delete'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Explore
 from .forms import ExploreForm
@@ -13,21 +14,45 @@ def explore(request, explore_id):
 def explore_gallery(request):
     explore_list = Explore.objects.order_by('explore_id')
     arguments = {'explore_list': explore_list}
-    return render(request, 'explore/explore-index.html', arguments)
+    return render(request, 'gallery/explore-index.html', arguments)
 
-@login_required(login_url="/common/login")
+@login_required(login_url='common:login')
 def explore_create(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = ExploreForm(request.POST, request.FILES)
         if form.is_valid():
             explore = form.save(commit=False)
             explore.explore_author = request.user
             explore.explore_date_created = timezone.now()
             explore.save()
-            return redirect('/gallery')
+            return redirect('gallery:explore-gallery')
 
-    return render(request, 'explore/explore-create.html')
+    return render(request, 'gallery/explore-create.html')
 
-@login_required(login_url="/common/login")
+@login_required(login_url='common:login')
 def explore_edit(request, explore_id):
-    return HttpResponse(f"{explore_id} - edit")
+    explore = Explore.objects.get(explore_id=explore_id)
+    arguments = {'explore': explore}
+    
+    if request.user != explore.explore_author:
+        message.error(request, 'You are not allowed to edit this explore!')
+        return redirect('gallery:explore-gallery')
+
+    if request.method == 'POST':
+        form = ExploreForm(request.POST, request.FILES, instance=explore)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery:explore-gallery')
+
+    return render(request, 'gallery/explore-edit.html', arguments)
+
+@login_required(login_url='common:login')
+def explore_delete(request, explore_id):
+    explore = Explore.objects.get(explore_id=explore_id)
+
+    if request.user != explore.explore_author:
+        message.error(request, 'You are not allowed to edit this explore!')
+        return redirect('gallery:explore-gallery')
+
+    explore.delete()
+    return redirect('gallery:explore-gallery')
