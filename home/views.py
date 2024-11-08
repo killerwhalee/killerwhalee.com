7592ba5dd399e89e3416558368734184f0cc65e6
killerwhalee <killerwhalee@kaist.ac.kr>
2024-11-08 18:01:55 +0000

    Fixed queryset

--- home/views.py
+++ home/views.py
@@ -10,8 +10,8 @@ def splash(request):
 def index(request):
     # Get random facts from database
     facts_q = FunFact.objects.order_by("?")
-    fact = facts_q[0] if facts_q.exists() else "아직 들려드릴 사실이 없습니다🥺"
+    fun_fact = facts_q[0] if facts_q.exists() else "아직 들려드릴 사실이 없습니다🥺"
 
-    context = {"fact": fact}
+    context = {"fact": fun_fact.fact}
 
     return render(request, "home/index.html", context)
