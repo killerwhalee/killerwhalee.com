78166d516be0fc30326e152aebebc0426bc68629
killerwhalee <killerwhalee@kaist.ac.kr>
2024-12-13 03:20:25 +0900

    Give random value for invalid input

--- projects/interactive/views.py
+++ projects/interactive/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 
+from uuid import uuid4
+
 
 def index(request):
     if request.method == "POST":
-        room_name = request.POST.get("room")
+        room_name: str = request.POST.get("room")
+
+        # Generate random room name if invalid
+        if not room_name.isalnum():
+            room_name = uuid4().hex[:8]
 
         return redirect("projects:interactive:room", room_name)
-
-    from django import urls
-
-    url_resolver = urls.get_resolver(urls.get_urlconf())
-    print(url_resolver.namespace_dict.keys())
 
     return render(request, "interactive/index.html")
 
