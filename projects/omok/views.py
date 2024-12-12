04e43cc070d3ec4357638617d800fe9a22773fc5
killerwhalee <killerwhalee@kaist.ac.kr>
2024-12-12 19:51:57 +0900

    Created empty template

--- projects/omok/views.py
+++ projects/omok/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Nothing Here Yet!")
+    return render(request, "empty.html")
