b8236cfb7cb398253a2c19af3a2fdaaaa3497f9f
killerwhalee <killerwhalee@kaist.ac.kr>
2024-12-15 00:36:33 +0900

    Added id221 projects

--- projects/id221/urls.py
+++ projects/id221/urls.py
@@ -6,4 +6,7 @@ app_name = "id221"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("project-1", views.project_1, name="project-1"),
+    path("project-2", views.project_2, name="project-2"),
+    path("project-3", views.project_3, name="project-3"),
 ]
--- projects/id221/views.py
+++ projects/id221/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "empty.html")
+    return render(request, "projects/id221/index.html")
+
+
+def project_1(request):
+    return render(request, "projects/id221/project-1.html")
+
+
+def project_2(request):
+    return render(request, "projects/id221/project-2.html")
+
+
+def project_3(request):
+    return render(request, "projects/id221/project-3.html")
