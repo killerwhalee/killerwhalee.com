ccc633c471d929b99981f0322d0bc470ba9c90f6
killerwhalee <killerwhalee@kaist.ac.kr>
2024-12-26 02:57:12 +0900

    Moved static/templates into each application

--- projects/id221/views.py
+++ projects/id221/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "projects/id221/index.html")
+    return render(request, "id221/index.html")
 
 
 def project_1(request):
-    return render(request, "projects/id221/project_1.html")
+    return render(request, "id221/project_1.html")
 
 
 def project_2(request):
-    return render(request, "projects/id221/project_2.html")
+    return render(request, "id221/project_2.html")
 
 
 def project_3(request):
-    return render(request, "projects/id221/project_3.html")
+    return render(request, "id221/project_3.html")
